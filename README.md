2c16b67db156decbd263cb0b0889be54930be8e2
Sushrut Sharma <sharmasushrut7@gmail.com>
2025-04-12 12:53:12 +0000

    Added a log statement to scripts.js file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 My first GitHub repo
+My first day of learning Git and it's basic commands. 
+Trying to learn some core commands.
---
+++ script.js
@@ -0,0 +1,2 @@
+console.log("Hello! Today is my first day of learning Git")
+console.log("Hello! This is my second day of learning Git")
